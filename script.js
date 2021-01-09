96cb887f055c82d1da21e1483edf877979996a55
emloraine <joh13021@byui.edu>
2021-01-09 14:43:11 -0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function outputyear() {
     document.getElementById("year").innerHTML = new Date().getFullYear();
 }
+function lastModified() {
+    document.getElementById("last").innerHTML = new Date(document.lastModified);
+}
